1753a174609b5ef81231328d1955fefd82edad64
Felipe Oliveira <felipeoes@usp.br>
2022-04-27 16:36:47 -0300

    Arquivos criados pelo gitupper <:)>

---
+++ beecrowd/443091/aceitas/Status/Accepted/1001_Extremamente_Básico.py
@@ -0,0 +1,6 @@
+A = int(input())
+B = int(input())
+
+out = "X = {value}".format(value = A+B)
+
+print(out)
---
+++ beecrowd/443091/aceitas/Status/Accepted/1003_Soma_Simples.py
@@ -0,0 +1,7 @@
+A = int(input())
+B = int(input())
+
+add = A + B
+
+out = "SOMA = {}".format(add)
+print(out)
